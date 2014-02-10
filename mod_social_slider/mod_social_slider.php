06552289a527de784b40fb8ff6b43e7bf9cb8bcd
Charlie <lodder15@hotmail.co.uk>
2014-02-10 13:54:15 +0000

    added top/bottom positioning parameter

--- mod_social_slider/mod_social_slider.php
+++ mod_social_slider/mod_social_slider.php
@@ -117,7 +117,7 @@ if ($position == "left")
 	$style = "";        
         $style .= 'ul#jj_sl_navigation { '
         . $position . ':0px;'
-        . 'top:' . $params->get('top') . 'px;'
+        . $params->get('top_bottom') . ':' . $params->get('top') . 'px;'
         . '}'
         . 'ul#jj_sl_navigation li:hover { '
         . 'margin-left: 0px;'
@@ -182,7 +182,7 @@ elseif ($position == "right")
 	$style = "";
         $style .= 'ul#jj_sl_navigation { '
                 . $position . ':0px;'
-                . 'top:' . $params->get('top') . 'px;'
+                . $params->get('top_bottom') . ':' . $params->get('top') . 'px;'
                 . '}'
                 . 'ul#jj_sl_navigation li:hover { '
                 . 'right: 0px;'
